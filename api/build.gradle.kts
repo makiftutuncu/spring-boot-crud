13b61de212be851e70502e530bd2db45a2905199
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2023-03-01 12:54:01 +0100

    Remove Spring Boot plugin

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -9,24 +9,23 @@ import java.time.LocalDate
 plugins {
     `java-library`
     `maven-publish`
-    id("io.spring.dependency-management") version "1.1.0"
-    id("org.springframework.boot") version "3.0.3"
     idea
     kotlin("jvm") version "1.8.0"
     signing
 }
 
 val junitVersion = "5.9.2"
+val springBootVersion = "3.0.3"
 val springdocVersion = "2.0.2"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-validation:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
